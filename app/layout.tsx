fe9cd04cebae2b171398271cccd6e34c003230c4
dev-hosein-moradi <moradih7379@gmail.com>
2024-08-06 17:41:43 +0330

    fix bug in next font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { ModalProvider } from "@/providers/modal-provider";
@@ -7,8 +6,6 @@ import { ModalProvider } from "@/providers/modal-provider";
 import "./globals.css";
 import { ToastProvider } from "@/providers/toast-provider";
 import { toast } from "react-toastify";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "E-Commerce Admin",
@@ -23,7 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={`font-sans`}>
           <ToastProvider />
           <ModalProvider />
           {children}
